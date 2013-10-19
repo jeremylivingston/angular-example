990a93cbbbd507513571794dee3ca6bf386424ca
Jeremy Livingston <jeremyjlivingston@gmail.com>
2013-10-19 01:23:47 +0000

    Add reject and approve modals

--- js/app.js
+++ js/app.js
@@ -13,10 +13,16 @@ applicantApp.factory('applicantProvider', ['$http', '$q', function($http, $q) {
             var deferred = $q.defer();
 
             setTimeout(function() {
-                var data = [
-                    { id: 1, first_name: 'Joe', last_name: 'Williams', approved: null },
-                    { id: 2, first_name: 'Bill', last_name: 'Witherspoon', approved: null }
-                ];
+                var data = {
+                    applicants: [
+                        { id: 1, first_name: 'Joe', last_name: 'Williams', approved: null, address: '1234 Test Lane', city: 'New York City', state: 'NY', zip: '10108', phone: '3131234567', email: 'joe@example.com', referrer: 'Web', high_school: 'New York High', gpa: '3.3', program: { id: 1, name: 'Fall 2014' } },
+                        { id: 2, first_name: 'Bill', last_name: 'Witherspoon', approved: null, address: '1234 Test Lane', city: 'New York City', state: 'NY', zip: '10108', phone: '3131234567', email: 'joe@example.com', referrer: 'Web', high_school: 'New York High', gpa: '3.3', program: { id: 1, name: 'Fall 2014' } }
+                    ],
+                    programs: [
+                        { id: 1, name: 'Fall 2014' },
+                        { id: 2, name: 'Winter 2014' }
+                    ]
+                };
 
                 deferred.resolve(data);
             }, 1000);
@@ -29,11 +35,40 @@ applicantApp.factory('applicantProvider', ['$http', '$q', function($http, $q) {
 var applicantControllers = angular.module('applicantControllers', []);
 
 applicantControllers.controller('ApplicantListCtrl', ['$scope', 'applicantProvider', function($scope, applicantProvider) {
-    var applicantPromise = applicantProvider.init();
+    $scope.applicants = [];
+    $scope.programs = [];
+    $scope.applicant = {};
 
-    applicantPromise.then(function(applicants) {
-        $scope.applicants = applicants;
+    // Initialize the lists
+    applicantProvider.init().then(function(data) {
+        $scope.applicants = data.applicants;
+        $scope.programs = data.programs;
     });
+
+    $scope.view = function(applicant) {
+        $scope.applicant = applicant;
+        $('#viewModal').modal('toggle');
+    };
+
+    $scope.toggleApproval = function(applicant) {
+        $scope.applicant = applicant;
+        $('#approvalModal').modal('toggle');
+    };
+
+    $scope.toggleRejection = function(applicant) {
+        $scope.applicant = applicant;
+        $('#rejectionModal').modal('toggle');
+    };
+
+    $scope.approve = function(applicant) {
+        // Make an HTTP call to accomplish this
+        applicant.approved = true;
+    };
+
+    $scope.reject = function(applicant) {
+        // Make an HTTP call to accomplish this
+        applicant.approved = false;
+    };
 
     $scope.displayApproved = function(applicant) {
         if (applicant == null) {
@@ -45,14 +80,5 @@ applicantControllers.controller('ApplicantListCtrl', ['$scope', 'applicantProvid
         }
 
         return (applicant.approved) ? 'Approved' : 'Rejected';
-    }
-}]);
-
-
-applicantControllers.controller('ApplicantViewCtrl', ['$scope', '$rootScope', 'applicantProvider', function($scope, $rootScope, applicantProvider) {
-    $scope.applicant = {};
-
-    $rootScope.$on('$routeChangeStart', function(evt, next, current) {
-        // I want this to set $scope.applicant and toggle the modal on
-    });
+    };
 }]);
